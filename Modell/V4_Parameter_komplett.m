c0ee48b00e32221b6d4405c5be0fc52ecea1e35c
Jonas Schmidt <jonas.schmidt@schanzer-racing.de>
2022-12-06 12:02:02 +0100

    Berechnung J im Parameterfile zentralisiert

--- Modell/V4_Parameter_komplett.m
+++ Modell/V4_Parameter_komplett.m
@@ -5,29 +5,27 @@
 %------------------------------------------------------------------------------------------------------------%
 % Elektrische Parameter GSM
 
-    L_A = 0.11*10^-3;  % [H] Ankerinduktivität
-    R_A = 0.36;        % [Ohm] Ankerwiderstand
-    C_Phi = 42.43e-3;  % [Nm/A] Maschinenkonstante    C_Phi = k_M
-    k_M = C_Phi;
+    L_A = 0.11*10^-3;   % [H] Ankerinduktivität
+    R_A = 0.36;         % [Ohm] Ankerwiderstand
+    C_Phi = 42.43e-3;   % [Nm/A] Maschinenkonstante    C_Phi = k_M
+    k_M = C_Phi;        % Kopie von C_Phi
 
 
 %------------------------------------------------------------------------------------------------------------%
 % Parameter Räder
 
     % Teflonräder
-    J_Rx = 1016e-5;         % Massenträgheitmoment je Rad Teflon
-    J = 164*10^-7 + 1.057*10^-6 + 2.333*10^-7 + 6.132*10^-6 + 4*1.016*10^-5;   % [kg*m^2] Trägheitsmoment 
-    m_Rx = 0.0298;          % Masse je Rad Teflon
-    mu = 0.1;   % Reibungskoeffizient Teflon
+    J_Rx = 1016e-5;             % [] Massenträgheitmoment je Rad Teflon
+    m_Rx = 0.0298;              % [kg] Masse je Rad Teflon
+    mu = 0.1;                   % [] Reibungskoeffizient Teflon
 
     % Stahlräder
-    %J_Rx = 4094e-5;       % Massenträgheitmoment je Rad Stahl
-    %J = 164*10^-7 + 1.057*10^-6 + 2.333*10^-7 + 6.132*10^-6 + 4*4.094*10^-5;   % [kg*m^2] Trägheitsmoment
-    %m_Rx =  0.13825       % Masse je Rad Stahl 
-    %mu = 0.3;   % Reibungskoeffizient Teflon
+    % J_Rx = 4094e-5;             % [] Massenträgheitmoment je Rad Stahl
+    % m_Rx =  0.13825             % [] Masse je Rad Stahl 
+    % mu = 0.3;                   % [] Reibungskoeffizient Teflon
 
-    r_Rx = 0.025;           % Radius je Rad Teflon & Stahl
-    m_Ersatz_Rad = (4 * J_Rx)/(r_Rx^2);
+    r_Rx = 0.025;               % [] Radius je Rad Teflon & Stahl
+    m_Ersatz_Rad = (4 * J_Rx)/(r_Rx^2); % [kg] Masse Ersatzrad
 
 
 %------------------------------------------------------------------------------------------------------------%
@@ -37,8 +35,8 @@
     c_w = 0.81;             % Luftwiderstandbeiwert Gehäuse und Batterie
     A = 115.5e-4;           % [m²] Querschnittsfläche
     phi_luft = 1.2;         % Luftdichte
-    m_w = 1.604 ;           %                   Masse (ohne Räder)
-    m_ges  = m_w + 4 * m_Rx;    % GesamtMasse
+    m_w = 1.604 ;           % Masse (ohne Räder)
+    m_ges  = m_w + 4 * m_Rx;    % Gesamtmasse
 
     J_EM = 164e-7;          % Massenträgheitmoment E-Maschine
     J_KR = 1057e-6;         % Massenträgheitmoment Kegelradgetriebe
@@ -50,6 +48,8 @@
     m_Ersatz_Antr = (J_EM + 2*J_KR+J_W + J_ZR)/(r_Zr^2);
     k = (m_trans + m_Ersatz_Antr + m_Ersatz_Rad)./m_trans;
     
+    J = J_EM + J_KR + J_W + J_ZR + 4*J_Rx;   % [kg*m^2] Trägheitsmoment 
+
 %------------------------------------------------------------------------------------------------------------%
 % Naturkonstanten
 
